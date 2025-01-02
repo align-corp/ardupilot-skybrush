1e99b5f5951cda325e730af1cd8a8803ac98a2eb
Andy Piper <github@andypiper.com>
2025-01-02 15:49:11 +0000

    AP_Scripting: scripting directory configuration for posix IO
    ensure SITL uses ./scripts

--- libraries/AP_Scripting/lua_common_defs.h
+++ libraries/AP_Scripting/lua_common_defs.h
@@ -11,11 +11,12 @@
 #endif // REPL_DIRECTORY
 
 #ifndef SCRIPTING_DIRECTORY
-  #if HAL_OS_FATFS_IO
+  // can't use HAL_OS_POSIX_IO here ebcause SITL assumes no APM prefix
+  #if HAL_OS_FATFS_IO || HAL_OS_LITTLEFS_IO
     #define SCRIPTING_DIRECTORY "/APM/scripts"
   #else
     #define SCRIPTING_DIRECTORY "./scripts"
-  #endif //HAL_OS_FATFS_IO
+  #endif // HAL_OS_FATFS_IO || HAL_OS_LITTLEFS_IO
 #endif // SCRIPTING_DIRECTORY
 
 #ifndef REPL_IN
